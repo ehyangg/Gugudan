4cde72196d3eaa8f029ff76ccc3e680d583fefea
user <user@soo>
2022-02-13 14:45:36 +0900

    1111

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -78,20 +78,3 @@ public class Gugudan {
 		}
 	}
 }
-=======
-//import java.util.Scanner;
-
-public class Gugudan {
-	public static void main(String[] args) {
-		int[] result = new int[9];
-		for(int j=2; j<10; j++) {
-		
-		for(int i = 0; i < result.length; i++) {
-			result[i] = j * (i + 1);
-		}
-		for(int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
-			}
-		}
-	}
-}
