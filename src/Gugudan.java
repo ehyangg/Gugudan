a7b3e93cefe6e7083673e2a38cefd9a2190ab0bb
qkqhz <qkqhz@DESKTOP-6VMTT9C>
2022-02-21 00:41:44 +0900

    maybe last..

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -111,10 +111,10 @@ public class Gugudan {
 	}
 	public static void main(String[] args) {
 		System.out.println("원하는 구구단은? : ");
-		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		for(int i=1; i<10; i++) {
-			System.out.println(number * i);
-		}
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		for(int i=1; i<10; i++)
+		System.out.println(number);
+		scanner.close();
 	}
 }
